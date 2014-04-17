4bb3511808f1a358dd0c709400b75af6d2669154
rolandtoth <contact@rolandtoth.hu>
2014-04-17 22:09:03 +0200

    fix #promoContainer z-index

--- promoBox.js
+++ promoBox.js
@@ -75,11 +75,11 @@ function promoBox(options) {
             css.type = 'text/css';
 
             styles = [
-                '#promoOverlay { position: fixed; width: 100%; height: 100%; top: 0; left: 0; zoom: 1; z-index: 990; background: rgba(0, 0, 0, 0.6); }',
-                '#promoContainer { position: fixed; width: 100%; height: 100%; text-align: center; top: 0; left: 0; }',
-                '#promoContent { position: relative; display: inline-block; z-index: 991; max-width: 80%; top: 16%; max-height: 80%; border-radius: 4px; border: 8px solid #fff; box-shadow: 0 0 2px #b4b4b4; }',
+                '#promoOverlay { position: fixed; width: 100%; height: 100%; top: 0; left: 0; zoom: 1; z-index: 9990; background: rgba(0, 0, 0, 0.6); }',
+                '#promoContainer { position: fixed; width: 100%; height: 100%; text-align: center; top: 0; left: 0; z-index: 9991; }',
+                '#promoContent { position: relative; display: inline-block; max-width: 80%; top: 16%; max-height: 80%; border-radius: 4px; border: 8px solid #fff; box-shadow: 0 0 2px #b4b4b4; }',
                 '#promoContent img { max-width: 100%; height: auto; display: block; }',
-                '#promoClose { position: absolute; top: 0; right: 0; display: block; line-height: 16px; height: 15px; text-align: right; padding: 13px 18px; color: #000; z-index: 99999; font-family: sans-serif; font-size: 17px; opacity: 0.6; transition: 0.12s all; }',
+                '#promoClose { position: absolute; top: 0; right: 0; display: block; line-height: 16px; height: 15px; text-align: right; padding: 13px 18px; color: #000; z-index: 9992; font-family: sans-serif; font-size: 17px; opacity: 0.6; transition: 0.12s all; }',
                 '#promoClose:hover { opacity: 1; cursor: pointer; }'
             ].join('');
 
